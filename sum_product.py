249679f87bdf514307e61fb27360a4b5a24bb861
pythonaian23 <pythonaian23@gmail.com>
2022-07-08 21:00:53 +0900

    sum_product

---
+++ sum_product.py
@@ -0,0 +1,13 @@
+def sum_product(num: int):
+    vals = []
+    for n in range(num, 0, -1):
+        #   a ,  a , b, c, ... =>    a**2    * ...
+        #  a+1, a-1, b, c, ... => (a**2 - 1) * ...
+        higher = num%n
+        low = num//n
+        vals.append((low**(n-higher)) * ((low+1)**higher))
+    return max(vals)
+
+
+for test in range(1, 20):
+    print(test, sum_product(test))
