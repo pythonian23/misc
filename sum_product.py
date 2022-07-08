907eff2872aee4c088ea163c020dbea2fffa41e4
pythonaian23 <pythonaian23@gmail.com>
2022-07-08 21:12:00 +0900

    black

--- sum_product.py
+++ sum_product.py
@@ -3,9 +3,9 @@ def sum_product(num: int):
     for n in range(num, 0, -1):
         #   a ,  a , b, c, ... =>    a**2    * ...
         #  a+1, a-1, b, c, ... => (a**2 - 1) * ...
-        higher = num%n
-        low = num//n
-        vals.append((low**(n-higher)) * ((low+1)**higher))
+        higher = num % n
+        low = num // n
+        vals.append((low ** (n - higher)) * ((low + 1) ** higher))
     return max(vals)
 
 
